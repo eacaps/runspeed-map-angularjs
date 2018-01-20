e322adc5f102d8140b1ed7d2395a6daaae347957
eacaps <eacaps@gmail.com>
2018-01-20 09:58:22 -0500

    use angular for scale component

--- src/app.js
+++ src/app.js
@@ -26,3 +26,27 @@ fetch('../data/activity_925308754.geojson').then((response) => {
   const geojson = JSON.parse(text);
   map.processGeojson(geojson);
 });
+
+import angular from 'angular';
+
+let app = () => {
+  return {
+    template: `<span>hi</span>`,
+    controller: 'AppCtrl',
+    controllerAs: 'app'
+  }
+};
+
+class AppCtrl {
+  constructor() {
+    this.url = 'https://github.com/preboot/angular-webpack';
+  }
+}
+
+const MODULE_NAME = 'app';
+
+angular.module(MODULE_NAME, [])
+  .directive('app', app)
+  .controller('AppCtrl', AppCtrl);
+
+export default MODULE_NAME;
---
+++ src/scalecomponent.js
@@ -0,0 +1,23 @@
+import angular from 'angular';
+
+let app = () => {
+  return {
+    template: `<span>hi</span>`,
+    controller: 'AppCtrl',
+    controllerAs: 'app'
+  }
+};
+
+class AppCtrl {
+  constructor() {
+    this.url = 'https://github.com/preboot/angular-webpack';
+  }
+}
+
+const MODULE_NAME = 'app';
+
+angular.module(MODULE_NAME, [])
+  .directive('app', app)
+  .controller('AppCtrl', AppCtrl);
+
+export default MODULE_NAME;
